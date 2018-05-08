bafe6ef6616f59e860e1c9fc50b80ab37fc8456e
samirgc <samirgc@DESKTOP-V1EFT37>
2018-05-09 01:17:50 +0530

    Add project files.

---
+++ XamarinCheck/XamarinCheck/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace XamarinCheck
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new XamarinCheck.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            AppCenter.Start("00118f07-1329-4ac0-8751-af84bc38b02e", typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
